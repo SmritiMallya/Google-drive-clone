0a494d2f5ae18eae8882b8aaf59c6e6c359c59c2
Smriti Sivakumar Mallya <113685211+SmritiMallya@users.noreply.github.com>
2025-08-25 18:55:00 +0530

    Delete backend/src directory

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { verifyUser } from "../middlewares/authMiddleware.js";
-import { getProfile } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/me", verifyUser, getProfile);
-
-export default router;
--- backend/src/utils/logger.js
+++
@@ -1,4 +0,0 @@
-export const logger = (msg, type = "info") => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${type.toUpperCase()}] ${timestamp}: ${msg}`);
-};
